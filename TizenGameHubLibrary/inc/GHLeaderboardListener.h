7dd0c6f757ba86ad0d171352041b1c5755ed105e
Huan88 <huan8886@gmail.com>
2014-03-28 17:24:37 +0900

    Done : - Leaderboard:data Class, Controller Class 설계 Todo : - leaderboard Listener 설계 - leaderboard 서버-Framework 통신 - response header에 따른 리스너 동작

---
+++ TizenGameHubLibrary/inc/GHLeaderboardListener.h
@@ -0,0 +1,22 @@
+/*
+ * GHLeaderboardListener.h
+ *
+ *  Created on: Mar 28, 2014
+ *      Author: Administrator
+ */
+
+#ifndef GHLEADERBOARDLISTENER_H_
+#define GHLEADERBOARDLISTENER_H_
+
+#include <GHLeaderboard.h>
+
+class GHLeaderboardListener {
+public:
+	GHLeaderboardListener();
+	virtual ~GHLeaderboardListener();
+
+
+
+};
+
+#endif /* GHLEADERBOARDLISTENER_H_ */
