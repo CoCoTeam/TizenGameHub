bab1ebb376729101699fe4eee5d7e9f3b8a5fcc4
CoCoTeam <dev.teamcoco@gmail.com>
2014-04-02 02:21:54 +0900

    JSON METHOD DECLARE ADD

--- TizenGameHubLibrary/inc/GHController.h
+++ TizenGameHubLibrary/inc/GHController.h
@@ -29,6 +29,21 @@ public:
 protected:
 	GHhttpClient httpPost;
 	virtual void OnTransactionReadyToRead(Tizen::Web::Json::IJsonValue* data) = 0;
+
+	// JSON FUNCTION - JSON Object
+	Tizen::Web::Json::JsonArray	  getJsonArrayByKey(Tizen::Web::Json::JsonObject jsonObj, String key);
+	Tizen::Web::Json::JsonObject  getJsonObjectByKey(Tizen::Web::Json::JsonObject jsonObj, String key);
+	Tizen::Base::String			  getStringByKey(Tizen::Web::Json::JsonObject jsonObj, String key);
+	int							  getIntByKey(Tizen::Web::Json::JsonObject jsonObj, String key);
+	bool						  getBoolByKey(Tizen::Web::Json::JsonObject jsonObj, String key);
+
+	// JSON FUNCTION - JSON Array
+	Tizen::Web::Json::JsonArray	  getJsonArrayByIndex(Tizen::Web::Json::JsonArray jsonArr, String key);
+	Tizen::Web::Json::JsonObject  getJsonObjectByIndex(Tizen::Web::Json::JsonArray jsonArr, String key);
+	Tizen::Base::String	  		  getStringByIndex(Tizen::Web::Json::JsonArray jsonArr, String key);
+	int	  						  getIntByIndex(Tizen::Web::Json::JsonArray jsonArr, String key);
+	bool	  					  getBoolByIndex(Tizen::Web::Json::JsonArray jsonArr, String key);
+
 };
 
 #endif /* GHCONTROLLER_H_ */
