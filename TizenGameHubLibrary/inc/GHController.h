7ccd82797a3ae481630bedc3e44e0fb7cfd7745b
hursujung <hur7411@hanmail.net>
2014-04-03 00:55:34 +0900

    Join완료

--- TizenGameHubLibrary/inc/GHController.h
+++ TizenGameHubLibrary/inc/GHController.h
@@ -31,18 +31,18 @@ protected:
 	virtual void OnTransactionReadyToRead(Tizen::Web::Json::IJsonValue* data) = 0;
 
 	// JSON FUNCTION - JSON Object
-	Tizen::Web::Json::JsonArray	  getJsonArrayByKey(Tizen::Web::Json::JsonObject jsonObj, String key);
-	Tizen::Web::Json::JsonObject  getJsonObjectByKey(Tizen::Web::Json::JsonObject jsonObj, String key);
-	Tizen::Base::String			  getStringByKey(Tizen::Web::Json::JsonObject jsonObj, String key);
-	int							  getIntByKey(Tizen::Web::Json::JsonObject jsonObj, String key);
-	bool						  getBoolByKey(Tizen::Web::Json::JsonObject jsonObj, String key);
+	Tizen::Web::Json::JsonArray	  getJsonArrayByKey(Tizen::Web::Json::JsonObject jsonObj, Tizen::Base::String key);
+	Tizen::Web::Json::JsonObject  getJsonObjectByKey(Tizen::Web::Json::JsonObject jsonObj, Tizen::Base::String key);
+	Tizen::Base::String			  getStringByKey(Tizen::Web::Json::JsonObject jsonObj, Tizen::Base::String key);
+	int							  getIntByKey(Tizen::Web::Json::JsonObject jsonObj, Tizen::Base::String key);
+	bool						  getBoolByKey(Tizen::Web::Json::JsonObject jsonObj, Tizen::Base::String key);
 
 	// JSON FUNCTION - JSON Array
-	Tizen::Web::Json::JsonArray	  getJsonArrayByIndex(Tizen::Web::Json::JsonArray jsonArr, String key);
-	Tizen::Web::Json::JsonObject  getJsonObjectByIndex(Tizen::Web::Json::JsonArray jsonArr, String key);
-	Tizen::Base::String	  		  getStringByIndex(Tizen::Web::Json::JsonArray jsonArr, String key);
-	int	  						  getIntByIndex(Tizen::Web::Json::JsonArray jsonArr, String key);
-	bool	  					  getBoolByIndex(Tizen::Web::Json::JsonArray jsonArr, String key);
+	Tizen::Web::Json::JsonArray	  getJsonArrayByIndex(Tizen::Web::Json::JsonArray jsonArr, Tizen::Base::String key);
+	Tizen::Web::Json::JsonObject  getJsonObjectByIndex(Tizen::Web::Json::JsonArray jsonArr, Tizen::Base::String key);
+	Tizen::Base::String	  		  getStringByIndex(Tizen::Web::Json::JsonArray jsonArr, Tizen::Base::String key);
+	int	  						  getIntByIndex(Tizen::Web::Json::JsonArray jsonArr, Tizen::Base::String key);
+	bool	  					  getBoolByIndex(Tizen::Web::Json::JsonArray jsonArr, Tizen::Base::String key);
 
 };
 
