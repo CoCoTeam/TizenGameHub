def73a0c8857e535899081ef32ca3fff98a937a1
Huan88 <huan8886@gmail.com>
2014-05-09 13:15:44 +0900

    c

--- CardPair/Debug/src/AppResourceId.d
+++ CardPair/Debug/src/AppResourceId.d
@@ -1,4 +1,4 @@
 src/AppResourceId.d: ../src/AppResourceId.cpp \
-  C:\Users\Administrator\Documents\workspace\tizen\CardPair\inc\AppResourceId.h
+  C:\Users\Administrator\Documents\workspace\tizen\TizenGameHub\CardPair\inc\AppResourceId.h
 
-C:\Users\Administrator\Documents\workspace\tizen\CardPair\inc\AppResourceId.h:
+C:\Users\Administrator\Documents\workspace\tizen\TizenGameHub\CardPair\inc\AppResourceId.h:
