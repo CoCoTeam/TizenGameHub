e4cfaefdd5e144a29fcac565db28b8912306bdbe
Huan88 <huan8886@gmail.com>
2014-05-15 13:35:24 +0900

    20140515 first

--- CardPair/Debug/src/AppResourceId.d
+++
@@ -1,4 +0,0 @@
-src/AppResourceId.d: ../src/AppResourceId.cpp \
-  C:\Users\JOHEEYEON\workspace\TizenGameHub\CardPair\inc\AppResourceId.h
-
-C:\Users\JOHEEYEON\workspace\TizenGameHub\CardPair\inc\AppResourceId.h:
